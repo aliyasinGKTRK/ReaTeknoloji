b7a03ba8d5547ec184d6819b440ae2aa8cf519f2
Aliyasin <Aliyasin@DESKTOP-K215639>
2023-02-11 17:43:34 +0300

    Add project files.

---
+++ DataAccess/Migrations/20230211143011_Create.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Create : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Klavye" },
+                    { 2, "Monitör" },
+                    { 3, "Mouse" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Description", "Price", "ProductName" },
+                values: new object[,]
+                {
+                    { 1, 1, "Oyuncu klavyesi", 500m, "Rampage" },
+                    { 2, 1, "RGB Oyuncu klavyesi", 250m, "Micropack" },
+                    { 3, 1, "Beyaz kablosuz Q klavye", 200m, "Everest" },
+                    { 4, 1, "Kablosuz Q klavye", 150m, "logitech" },
+                    { 5, 2, "IPS Ekran Monitör", 6000m, "Huawei" },
+                    { 6, 2, "Curved oyuncu monitörü", 6000m, "Samsung" },
+                    { 7, 2, "IPS Ekran moniör", 5000m, "Samsung" },
+                    { 8, 2, "IPS Ekran moniör", 5000m, "LG" },
+                    { 9, 2, "Cruved moniör", 4500m, "LG" },
+                    { 10, 3, "Kablosuz mouse", 500m, "logitech" },
+                    { 11, 3, "Kablosuz mouse", 500m, "Everest" },
+                    { 12, 3, "Kablosuz oyunucu mouse", 1500m, "Rampage" },
+                    { 13, 3, "Kablosuz oyunucu mouse", 1500m, "Rampage" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                column: "CategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
---
+++ DataAccess/ReaContext.cs
@@ -0,0 +1,51 @@
+﻿using Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class ReaContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB; database=ReaDb;Trusted_Connection=false");
+        }
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                    modelBuilder.Entity<Category>()
+              .HasData(
+                  new Category() { Id = 1, Name = "Klavye" },
+                  new Category() { Id = 2, Name = "Monitör" },
+                  new Category() { Id = 3, Name = "Mouse" }
+                      );
+
+
+            modelBuilder.Entity<Product>()
+                .HasData(
+                    new Product() { Id = 1, CategoryId = 1, ProductName = "Rampage", Description = "Oyuncu klavyesi", Price = 500 },
+                    new Product() { Id = 2, CategoryId = 1, ProductName = "Micropack", Description = "RGB Oyuncu klavyesi", Price = 250 },
+                    new Product() { Id = 3, CategoryId = 1, ProductName = "Everest", Description = "Beyaz kablosuz Q klavye", Price = 200 },
+                    new Product() { Id = 4, CategoryId = 1, ProductName = "logitech", Description = "Kablosuz Q klavye", Price = 150 },
+                    new Product() { Id = 5, CategoryId = 2, ProductName = "Huawei", Description = "IPS Ekran Monitör", Price = 6000 },
+                    new Product() { Id = 6, CategoryId = 2, ProductName = "Samsung", Description = "Curved oyuncu monitörü", Price = 6000 },
+                    new Product() { Id = 7, CategoryId = 2, ProductName = "Samsung", Description = "IPS Ekran monitör", Price = 5000 },
+                    new Product() { Id = 8, CategoryId = 2, ProductName = "LG", Description = "IPS Ekran monitör", Price = 5000 },
+                    new Product() { Id = 9, CategoryId = 2, ProductName = "LG", Description = "Cruved monitör", Price = 4500 },
+                    new Product() { Id = 10, CategoryId = 3, ProductName = "logitech", Description = "Kablosuz mouse", Price = 500 },
+                    new Product() { Id = 11, CategoryId = 3, ProductName = "Everest", Description = "Kablosuz mouse", Price = 500 },
+                    new Product() { Id = 12, CategoryId = 3, ProductName = "Rampage", Description = "Kablosuz oyunucu mouse", Price = 1500 },
+                    new Product() { Id = 13, CategoryId = 3, ProductName = "Rampage", Description = "Kablosuz oyunucu mouse", Price = 1500 }
+
+                );
+            
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
